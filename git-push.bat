eb9586e61a787f6ed205b979c43ba29903d65f6e
Enz <Enzear@mail.ru>
2022-12-21 18:55:32 +0300

    new commit

--- git-push.bat
+++ git-push.bat
@@ -1,6 +1,6 @@
 git add .
 git commit -m "new commit"
 git branch -M dev
-git remote add speakup-api https://github.com/brizgalka/speakup-desktop.git
+git remote add speakup-api https://github.com/brizgalka/speakup-api.git
 git pull speakup-api dev
 git push speakup-api dev
